3a511c1f8eb95ab646b09753c73616769a8bfa31
Ramdani <ramdani@Ramdanis-MacBook-Air.local>
2023-04-27 15:24:18 +0800

    fix: update todo indices

--- app/indices/todo.go
+++ app/indices/todo.go
@@ -5,12 +5,11 @@ import (
 )
 
 type Todo struct {
-	ID          uint        `gorm:"primarykey" json:"id"`
-	UserId      uint        `gorm:"not null" json:"user_id"`
-	Title       string      `gorm:"size:150;not null" json:"title"`
-	Description string      `gorm:"size:150;not null" json:"description"`
-	IsDone      bool        `json:"is_done"`
-	User        models.User `gorm:"foreignKey:UserId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	ID          uint   `gorm:"primarykey" json:"id"`
+	UserId      uint   `gorm:"not null" json:"user_id"`
+	Title       string `gorm:"size:150;not null" json:"title"`
+	Description string `gorm:"size:150;not null" json:"description"`
+	IsDone      bool   `json:"is_done"`
 	models.BaseModel
 }
 
